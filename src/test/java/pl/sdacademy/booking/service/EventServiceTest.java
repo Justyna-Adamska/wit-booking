b9785e47c643003c9f39b871d49fc9bfa120e1f7
Justyna Adamska <justyna.anna.adamska@gmail.com>
2023-09-18 21:59:59 +0200

    added event service test

---
+++ src/test/java/pl/sdacademy/booking/service/EventServiceTest.java
@@ -0,0 +1,62 @@
+package pl.sdacademy.booking.service;
+
+import org.junit.jupiter.api.Test;
+import pl.sdacademy.booking.data.EventEntity;
+import pl.sdacademy.booking.data.ItemEntity;
+import pl.sdacademy.booking.model.EventDto;
+import pl.sdacademy.booking.repository.EventRepository;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class EventServiceTest {
+    private EventService sut;
+
+    @Test
+    void shouldResultAllItemsInDbAsListOfDto() {
+        sut = new EventService(new EventServiceTest.TestEventRepository());
+
+        List<EventDto> result = sut.findEvents();
+
+        assertThat(result).hasSize(1);
+        EventDto first = result.get(0);
+        // liczba ponizszych asercji sygnalizuje, ze klasa moze miec za duzo odpowiedzialnosci
+        // powinna zostac podzielona na bardziej specjalistyczne klasy
+        assertThat(first.getName()).isEqualTo("Usuwanie przebarwień twarz");
+        assertThat(first.getPrice()).isEqualTo(BigDecimal.valueOf(130.0));
+        assertThat(first.getFromTime()).isEqualTo(LocalDateTime.of(2023, Month.SEPTEMBER, 13, 12, 00, 00));
+        assertThat(first.getToTime()).isEqualTo(LocalDateTime.of(2023, Month.SEPTEMBER, 13, 12, 50, 00));
+
+
+    }
+
+    public static class TestEventRepository implements EventRepository {
+
+        @Override
+        public List<EventEntity> findAll() {
+
+
+            ItemEntity first = new ItemEntity();
+            first.setId(1l);
+            first.setName("Usuwanie przebarwień twarz");
+
+            first.setPrice(BigDecimal.valueOf(130.0));
+            EventEntity pierwszy = new EventEntity();
+            pierwszy.setItem(first);
+            pierwszy.setFrom(LocalDateTime.of(2023, Month.SEPTEMBER, 13, 12, 00, 00));
+            pierwszy.setTo(LocalDateTime.of(2023, Month.SEPTEMBER, 13, 12, 50, 00));
+            return List.of(pierwszy);
+        }
+
+
+    }
+
+
+}
